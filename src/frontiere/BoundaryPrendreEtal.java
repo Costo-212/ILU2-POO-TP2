36f0bb9d8d11efb6b18afb63841c0d2687d97599
Radouane Akabi <kbr5187a@ensfsi.onmicrosoft.com>
2026-04-03 14:26:39 +0200

    Prendre Etal DONE

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -33,6 +33,15 @@ public class BoundaryPrendreEtal {
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un étal pour vous!");
+		System.out.println("Il me faudrait quelques renseignments : ");
+		String produit = Clavier.entrerChaine("Quel produit souhaitez-vous vendre ?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if(numeroEtal !=-1) {
+			System.out.println("Le vendeur " + nomVendeur + "s'est insatllée à l'étal n°" + numeroEtal + 1);
+		}
+		
+	}
 	}
 }
